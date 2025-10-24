af69a1cea0565565c7bc40c62188b7843c0dd803
Abhishekyadav3824 <aradaabhishek@gmail.com>
2025-10-24 10:59:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const timer=document.getElementById("timer")
+setInterval(()=>{
+    const currTime=new Date()
+    timer.innerText=(currTime.toLocaleDateString())+", "+(currTime.toLocaleTimeString())
+},1000)
